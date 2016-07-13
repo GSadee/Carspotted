a1f4aa2a80eda4ad74b68461fef0e0fa8c34fa9b
Grzegorz Sadowski <sadowskigp@gmail.com>
2016-07-13 23:03:21 +0200

    Init Symfony2 project

---
+++ src/AppBundle/Controller/HomepageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author Grzegorz Sadowski <sadowskigp@gmail.com>
+ */
+class HomepageController extends Controller
+{
+    /**
+     * @param Request $request
+     * 
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render(
+            ':homepage:index.html.twig',
+            [
+                'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+            ]
+        );
+    }
+}
