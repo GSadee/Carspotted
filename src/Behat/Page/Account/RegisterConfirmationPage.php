4dd1c7e62da8d2dbaf84f5c268c9beb4b6017447
Grzegorz Sadowski <sadowskigp@gmail.com>
2016-08-08 00:37:59 +0200

    [Behat] Add register and register confirmation pages

---
+++ src/Behat/Page/Account/RegisterConfirmationPage.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Behat\Page\Account;
+
+use Behat\Page\SymfonyPage;
+
+/**
+ * @author Grzegorz Sadowski <sadowskigp@gmail.com>
+ */
+class RegisterConfirmationPage extends SymfonyPage implements RegisterConfirmationPageInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getRouteName()
+    {
+        return 'fos_user_registration_confirmed';
+    }
+
+    /**
+     * @param string $text
+     *
+     * @return bool
+     */
+    public function hasText($text)
+    {
+        $contentText = $this->getElement('content')->getText();
+
+        return stripos($contentText, $text) !== false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getDefinedElements()
+    {
+        return array_merge(parent::getDefinedElements(), [
+            'content' => 'section#main',
+        ]);
+    }
+}
